57ba75f78482f2833456c398c63cc4dd45e5e08c
Michael Naquin <mnaquin1@yahoo.com>
2020-08-28 19:37:38 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -9,3 +9,7 @@ return `Happy holidays, ${name}!`
 function happyCustomHolidayTo(holiday, name) {
   return `Happy ${holiday}, ${name}!`
 }
+
+function holidayCountdown(holiday, days){
+return `It\'s ${days} until ${holiday}!`
+}
